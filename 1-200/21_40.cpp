c9053b6749c687d294d463b3809f631868c48ef4
DYnamoX513 <923948485@qq.com>
2021-03-02 16:04:17 +0800

    No.23, 24, 25, 304 (48 solved)

--- 1-200/21_40.cpp
+++ 1-200/21_40.cpp
@@ -1,8 +1,11 @@
 ﻿//
 // Created by Pochita on 2021/3/1.
 //
+#include <algorithm>
 #include <ListNode.h>
+#include <map>
 #include <vector>
+#include <stack>
 #include <string>
 
 using namespace std;
@@ -44,9 +47,106 @@ public:
                 generate(strings, s + '(', left - 1, right);
         }
     }
+
+    //map内部为红黑树
+    //优先队列(priority queue)，内部结构为堆，维护最多k个最前面的节点，每次出队一个最小的节点
+    ListNode* mergeKLists_23(vector<ListNode*>& lists) {
+        //将所有节点按值大小分组排列，再依次连接起来
+        map<int, ListNode *> orderedByVal;
+        map<int, ListNode *> heads;
+        for (auto list:lists) {
+            while (list) {
+                auto ptr = list;
+                list = list->next;
+                auto exist = orderedByVal.find(ptr->val);
+                if (exist == orderedByVal.end()) {
+                    orderedByVal[ptr->val] = ptr;
+                    heads[ptr->val] = ptr;
+                } else {
+                    exist->second->next = ptr;
+                    exist->second = ptr;
+                }
+                ptr->next = nullptr;
+            }
+        }
+        if (orderedByVal.empty()) return nullptr;
+        auto iter = orderedByVal.begin();
+        while (true) {
+            auto ptr = iter->second;
+            if (++iter != orderedByVal.end()) ptr->next = heads[iter->first];
+            else break;
+        }
+
+        return heads.begin()->second;
+    }
+
+    ListNode* swapPairs_24(ListNode* head) {
+        if (head == nullptr) return head;
+        auto ptr1 = head, ptr2 = ptr1->next;
+        if (ptr2 == nullptr) return head;
+        auto newHead = new ListNode(0, head), pre = newHead;
+        while (ptr1 && ptr2) {
+            pre->next = ptr2;
+            pre = ptr1;
+            ptr1->next = ptr2->next;
+            ptr2->next = ptr1;
+            ptr1 = ptr1->next;
+            if (ptr1) ptr2 = ptr1->next;
+        }
+
+        return newHead->next;
+    }
+
+    //使用了栈，空间O(k)
+    ListNode* reverseKGroup_25(ListNode* head, int k) {
+        if (k == 1 || head == nullptr || head->next == nullptr) return head;
+        stack<ListNode *> st;
+        auto newHead = new ListNode(0), pre = newHead, ptr = head;
+        while (true) {
+            pre->next = ptr;
+            for (int i = 0; i < k; ++i) {
+                if (ptr == nullptr) return newHead->next;
+                st.push(ptr);
+                ptr = ptr->next;
+            }
+            while (!st.empty()) {
+                pre->next = st.top();
+                pre = st.top();
+                st.pop();
+            }
+        }
+    }
+
+    //空间O(1)
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (k == 1 || head == nullptr || head->next == nullptr) return head;
+        auto reverse = [](ListNode *h, ListNode *t) {
+            ListNode *ptr;
+            while (h != t) {
+                ptr = h;
+                h = h->next;
+                ptr->next = t->next;
+                t->next = ptr;
+            }
+        };
+        auto newHead = new ListNode(0, head), h = newHead, t = h;
+        while (true) {
+            for (int i = 0; i < k; ++i) {
+                if (t->next == nullptr) return newHead->next;
+                t = t->next;
+            }
+            auto reverseHead = h->next;
+            reverse(reverseHead, t);
+            h->next = t;
+            h = t = reverseHead;
+        }
+    }
 };
 
 int main(){
     Solution s;
-    s.generateParenthesis_22(3);
+    vector<ListNode *> k({new ListNode(1,new ListNode(2)), new ListNode(2,new ListNode(3))});
+    s.mergeKLists_23(k);
+//    s.generateParenthesis_22(3);
 }
+
--- 201-400/301_320.cpp
+++ 201-400/301_320.cpp
@@ -21,9 +21,63 @@ public:
     int sumRange(int i, int j) const {
         return sums[j + 1] - sums[i];
     }
+
+    ~NumArray() {
+        delete sums;
+    }
+};
+
+//No.304
+class NumMatrix {
+public:
+    int **sums = nullptr;
+    int sRow;
+    int sCol;
+
+    explicit NumMatrix(vector<vector<int>> &matrix) {
+        int row = matrix.size();
+        if (row == 0) return;
+        int col = matrix[0].size();
+        if (col == 0) return;
+
+        sRow = row +1;
+        sCol = col +1;
+        sums = new int *[sRow];
+        sums[0] = new int[sCol]();
+        for (int i = 0, j = 1; i < row; ++i, ++j) {
+            sums[j] = new int[sCol];
+            sums[j][0] = 0;
+            int m = 0;
+            for (int k = 0; k < col; ++k) {
+                m =
+                sums[j][k + 1] = sums[j][k] + sums[j - 1][k + 1] - sums[j - 1][k] + matrix[i][k];
+            }
+        }
+    }
+
+    int sumRegion(int row1, int col1, int row2, int col2) const {
+        return sums[row2 + 1][col2 + 1] - sums[row1][col2 + 1] - sums[row2 + 1][col1] + sums[row1][col1];
+    }
+    
+    ~NumMatrix() {
+        if (sums == nullptr) return;
+        for (int i = 0; i < sRow; ++i) {
+            delete []sums[i];
+        }
+        delete []sums;
+    }
 };
 
 class Solution {
 public:
 
 };
+
+int main() {
+    Solution s;
+    vector<vector<int>> test({
+                                     {},
+    });
+    NumMatrix * n = new NumMatrix(test);
+    n->sumRegion(0,2,1,5);
+}
