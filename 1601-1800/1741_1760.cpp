a3c1697c501af63c0f20749cfb74deb6d70a4ec9
New Pluto <923948485@qq.com>
2021-06-03 00:06:44 +0800

    No.523, 1744 (106)

---
+++ 1601-1800/1741_1760.cpp
@@ -0,0 +1,25 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<bool> canEat_1744(vector<int>& candiesCount,
+                             vector<vector<int>>& queries) {
+        vector<bool> result;
+        int length = candiesCount.size();
+        vector<long long> sumBefore(length + 1);
+        sumBefore[0] = 0;
+        for (int i = 0; i < length; i++)
+            sumBefore[i + 1] = sumBefore[i] + candiesCount[i];
+
+        for (const auto& query : queries) {
+            long long leastEat = query[1],
+                      mostEat = (long long)(query[1] + 1) * query[2],
+                      candyBefore = sumBefore[query[0]];
+            result.emplace_back(candyBefore < mostEat &&
+                                candyBefore + candiesCount[query[0]] >
+                                    leastEat);
+        }
+        return result;
+    }
+};
---
+++ 401-600/521_540.cpp
@@ -0,0 +1,24 @@
+﻿#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool checkSubarraySum_523(vector<int>& nums, int k) {
+        unordered_map<int, int> mod;
+        int sum = nums[0] % k;
+        mod[sum] = 1;
+        int length = nums.size();
+        for (int i = 1; i < length; ++i) {
+            sum += nums[i];
+            sum %= k;
+            if (sum == 0) return true;
+            if (mod.count(sum)) {
+                int m = mod[sum];
+                if (i > m) return true;
+            } else
+                mod[sum] = i + 1;
+        }
+        return false;
+    }
+};
