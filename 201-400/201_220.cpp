48263212a7fce0cf33c5403538554eff69eab2a2
DYnamoX513 <923948485@qq.com>
2021-06-06 01:10:32 +0800

    No.203 (108)

---
+++ 201-400/201_220.cpp
@@ -0,0 +1,22 @@
+﻿//
+// Created by Pochita on 2021/6/6.
+//
+
+#include <ListNode.h>
+//using namespace std;
+
+class Solution {
+public:
+    ListNode* removeElements_203(ListNode* head, int val) {
+        ListNode *pre = new ListNode(0, head), *ptr = pre;
+        while(ptr->next != nullptr) {
+            if(ptr->next->val == val)
+                ptr->next = ptr->next->next;
+            else
+                ptr = ptr->next;
+        }
+        auto result = pre->next;
+        delete pre;
+        return result;
+    }
+};
